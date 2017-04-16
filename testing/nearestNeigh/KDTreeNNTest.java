b8fd09a04d20520b4485580eb7327cbb125587fd
rhysellwood <s3491191@student.rmit.edu.au>
2017-04-16 22:25:20 +1000

    test for report

---
+++ testing/nearestNeigh/KDTreeNNTest.java
@@ -0,0 +1,24 @@
+package nearestNeigh;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by rhysellwood on 12/4/17.
+ */
+public class KDTreeNNTest {
+
+
+    @Test
+    public void test() {
+
+
+    }
+
+    @Test
+    public void test2() {
+        int n = 1/2;
+        System.out.println(n);
+    }
+}
